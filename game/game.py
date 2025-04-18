f56fa46f27b1d1085dfa22dbb64675447fea00b3
Priyanshi2319 <priyanshimalviyaa@gmail.com>
2025-04-18 12:52:33 +0530

    DONE

--- game/game.py
+++ game/game.py
@@ -6,13 +6,13 @@ class Game:
         self.board = Board()
         self.players = []
         self.current_player_index = 0
-        
+
 # Game class handles the drama between X and O 
 
     def setup_players(self):
         name1 = input("Enter Player 1 name: ")
         name2 = input("Enter Player 2 name: ")
-        self.players = [Player(name1, 'X'), Player(name2, 'O')]
+        self.players = [ Player(name1, 'X'), Player(name2, 'O')]
 
     def switch_player(self):
         self.current_player_index = 1 - self.current_player_index
@@ -22,17 +22,17 @@ class Game:
         self.board.print_board()
 
         while True:
-            player = self.players[self.current_player_index]
+            player =  self.players[self.current_player_index]
             print(f"{player.name}'s turn ({player.symbol})")
             try:
                 row = int(input("Enter row (0-2): "))
                 col = int(input("Enter col (0-2): "))
             except ValueError:
-                print("Invalid input. Try again.")
+                print  ("Invalid input. Try again.")
                 continue
 
             if not self.board.place_move(row, col, player.symbol):
-                print("Invalid move. Try again.")
+                print ("Invalid move. Try again.")
                 continue
 
             self.board.print_board()
